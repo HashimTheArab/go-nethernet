0de0b87a130638bfef470817a6408be27f3c44ff
lactyy2 <284460485+lactyy2@users.noreply.github.com>
2026-09-12 22:59:43 +0900

    conn.go: Reject malformed error signals

--- conn.go
+++ conn.go
@@ -426,11 +426,7 @@ func (conn *Conn) handleSignal(signal *Signal) error {
 	case SignalTypeError:
 		code, err := strconv.ParseUint(signal.Data, 10, 32)
 		if err != nil {
-			e := fmt.Errorf("parse error code: %w", err)
-			if err := conn.close(fmt.Errorf("nethernet: remote peer notified connection failure (invalid code: %q)", signal.Data)); err != nil {
-				e = errors.Join(fmt.Errorf("close: %w", err), e)
-			}
-			return e
+			return fmt.Errorf("parse error code: %w", err)
 		}
 		if err := conn.close(fmt.Errorf("nethernet: remote peer notified connection failure (code: %d)", code)); err != nil {
 			return fmt.Errorf("close: %w", err)
